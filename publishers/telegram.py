df7d112191ce39a7a4bcc89a0606274b10cb777c
gtmp047 <g.t.m.p@yandex.ru>
2019-07-26 22:34:37 +0300

    continue tomorow

--- publishers/telegram.py
+++ publishers/telegram.py
@@ -1,7 +1,17 @@
-class TelegramPublisher():
-    def __init__(self, t_channel=None, config=None):
-        pass
+import telepot
+from utils.interactions import CONFIG
 
+
+# todo make mongo saver
+
+class TelegramPublisher():
+    def __init__(self, t_channel=None, config=CONFIG):
+        self.config = config
+        self.telepot_bot = telepot.Bot(self.config['telegram']['token'])
+        self.t_channel = t_channel
+
+    def send_document(self, data):
+        pass
 
 if __name__ == '__main__':
     TelegramPublisher()
