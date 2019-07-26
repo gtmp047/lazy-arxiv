86e33c22cb9306d55ac4cd2d3b2d207417f1a7c2
Anton O. Burygin <a.burygin@norsi-trans.ru>
2019-07-26 17:05:09 +0300

    update skeleton

---
+++ publishers/telegram.py
@@ -0,0 +1,7 @@
+class TelegramPublisher():
+    def __init__(self, t_channel=None, config=None):
+        pass
+
+
+if __name__ == '__main__':
+    TelegramPublisher()
---
+++ utils/interactions.py
@@ -0,0 +1,23 @@
+from os.path import dirname, join, exists, abspath, normpath
+
+import yaml
+from dotted.collection import DottedDict
+
+PROJECT_NAME = 'lazy_arxiv'
+PROJECT_ROOT_DIR_PATH = abspath(join(dirname(__file__), '..', '..'))
+
+CONFIG_FILE_NAME = 'configs/admin.conf'
+LOG_FILE_NAME = f'{PROJECT_NAME}.log'
+
+CONFIG_FILE_PATH = join(PROJECT_ROOT_DIR_PATH, PROJECT_NAME, CONFIG_FILE_NAME)
+
+LOG_DIR_PATH = join(PROJECT_ROOT_DIR_PATH, PROJECT_NAME, 'logs')
+LOG_FILE_PATH = join(LOG_DIR_PATH, LOG_FILE_NAME)
+
+DATA_DIR_PATH = join(PROJECT_ROOT_DIR_PATH, PROJECT_NAME, 'data')
+
+assert exists(CONFIG_FILE_PATH), 'CONFIG_FILE_PATH not detected!'
+assert exists(LOG_DIR_PATH), 'LOG_FILE_PATH not detected!'
+
+with open(CONFIG_FILE_PATH) as conf_file:
+    CONFIG = DottedDict(yaml.safe_load(conf_file))
