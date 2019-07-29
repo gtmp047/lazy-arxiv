21ecc5e8bd2b030f67cbcd0d9dbebe3d7ca43b47
gtmp047 <g.t.m.p@yandex.ru>
2019-07-29 23:03:13 +0300

    need to rethink project

--- publishers/telegram.py
+++ publishers/telegram.py
@@ -1,17 +1,29 @@
 import telepot
 from utils.interactions import CONFIG
+from urllib3.contrib.socks import SOCKSProxyManager
+from telepot.api import _default_pool_params, _onetime_pool_params
 
 
 # todo make mongo saver
 
+def set_telepot_socks_proxy(url, username=None, password=None):
+    # from https://github.com/nickoala/telepot/pull/386
+    telepot.api._onetime_pool_spec = (
+    SOCKSProxyManager, dict(proxy_url=url, username=username, password=password, **_onetime_pool_params))
+    telepot.api._pools['default'] = SOCKSProxyManager(url, username=username, password=password, **_default_pool_params)
+
+
 class TelegramPublisher():
     def __init__(self, t_channel=None, config=CONFIG):
         self.config = config
-        self.telepot_bot = telepot.Bot(self.config['telegram']['token'])
+        set_telepot_socks_proxy(f"socks5://orbtl.s5.opennetwork.cc:999", '448215182', 'UaP96Qhk')
+        self.telepot_bot = telepot.Bot(self.config.telegram.token)
         self.t_channel = t_channel
+        self.telepot_bot.sendMessage(self.t_channel, "test")
 
     def send_document(self, data):
         pass
 
+
 if __name__ == '__main__':
-    TelegramPublisher()
+    TelegramPublisher(CONFIG.telegram.chanel_name)
--- utils/interactions.py
+++ utils/interactions.py
@@ -3,7 +3,7 @@ from os.path import dirname, join, exists, abspath, normpath
 import yaml
 from dotted.collection import DottedDict
 
-PROJECT_NAME = 'lazy_arxiv'
+PROJECT_NAME = 'lazy-arxiv'
 PROJECT_ROOT_DIR_PATH = abspath(join(dirname(__file__), '..', '..'))
 
 CONFIG_FILE_NAME = 'configs/admin.conf'
@@ -17,7 +17,7 @@ LOG_FILE_PATH = join(LOG_DIR_PATH, LOG_FILE_NAME)
 DATA_DIR_PATH = join(PROJECT_ROOT_DIR_PATH, PROJECT_NAME, 'data')
 
 assert exists(CONFIG_FILE_PATH), 'CONFIG_FILE_PATH not detected!'
-assert exists(LOG_DIR_PATH), 'LOG_FILE_PATH not detected!'
+# assert exists(LOG_DIR_PATH), 'LOG_FILE_PATH not detected!'
 
 with open(CONFIG_FILE_PATH) as conf_file:
     CONFIG = DottedDict(yaml.safe_load(conf_file))
