35477b3f78d08296f38238bc105aebcc86c275d6
Anton O. Burygin <a.burygin@norsi-trans.ru>
2019-07-29 13:07:25 +0300

    complete arxiv crawler

--- crawlers/arxiv.py
+++ crawlers/arxiv.py
@@ -1,4 +1,6 @@
 import xmltodict
+from typing import List
+
 try:
     from urllib.parse import urljoin, urlencode
 except ImportError:
@@ -9,19 +11,36 @@ except ImportError:
     from urllib2 import urlopen
 
 
+def get_first(val: list):
+    if val and isinstance(val, list):
+        return val[0]
+    return val
+
+
 class ArxivCrawler:
-    def __init__(self):
-        self.main_url = f'http://export.arxiv.org/api/query?search_query=all:image+segmentation&start=0&max_results=10'
-        data = urlopen(self.main_url).read()
-        a = xmltodict.parse(data)
-        print(data)
+    def __init__(self, search_query: str):
+        query = self.build_query_string(search_query)
+        self.query_url = f'http://export.arxiv.org/api/query?{query}'
+        self.raw_data = xmltodict.parse(urlopen(self.query_url).read())
+        self.parsed_data = ArxivCrawler.get_entry_infos(self.raw_data)
 
+    @staticmethod
+    def get_entry_infos(entry_list: List):
+        res = []
+        for item in entry_list:
+            item['author'] = [i.get('name') for i in item['author']]
+            item['pdf_link'] = get_first([i.get('@href') for i in item['link'] if i.get('@type') == 'application/pdf'])
+            item['category'] = [i.get('@term') for i in item['category']]
+            res.append(item)
+        return res
 
-if __name__ == '__main__':
-    a = ArxivCrawler()
-
-
-    url = '?search_query=all:segmentation&start=0&max_results=10'
-    data = urlopen(url).read()
-    a = xmltodict.parse(data)
-    print(data)
+    def build_query_string(self, search_query: str, start: int = 0, max_results: int = 50,
+                           sortBy: str = 'submittedDate', sortOrder: str = 'descending'):
+        data = {
+            'search_query': search_query,
+            'start': start,
+            'max_results': max_results,
+            'sortBy': sortBy,
+            'sortOrder': sortOrder,
+        }
+        return urlencode(data)
